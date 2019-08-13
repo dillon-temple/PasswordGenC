e795ded61e236551300ced960d0b27a26ecb8a2b
Dillon Temple <momo@Dillons-MacBook-Pro.local>
2019-08-13 12:02:43 -0700

    Done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PasswordGen.Models;
+using System.Text;
+
+namespace PasswordGen.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            StringBuilder result = new StringBuilder();
+            Random random = new Random();
+
+            int i = 0;
+            while (i < 14)
+            {
+                result.Append(characters[random.Next(0, characters.Length)]);
+                i++;
+            }
+            ViewBag.result = result;
+            return View();
+        }
+    }
+}
